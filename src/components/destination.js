b707534d220494acd65df98b45426dab83ad3a0d
Lalu-Swamy <120311589+Lalu-Swamy@users.noreply.github.com>
2022-12-15 18:07:54 +0000

    my changes

--- src/components/destination.js
+++ src/components/destination.js
@@ -4,7 +4,10 @@ import Raanofkutch from '../Assets/Raanofkutch.jpg'
 import raan from '../Assets/raan.jpg'
 import kyoto from '../Assets/kyoto.jpg'
 import aboutpic from '../Assets/aboutpic.jpg'
-
+import paris1 from '../Assets/paris1.jpg'
+import paris2 from '../Assets/paris2.jpg'
+import tajj from '../Assets/tajj.jpg'
+import tajmahal from '../Assets/tajmahal.jpg'
 export default function Destination(){
     return(
         <div className="destination">
@@ -12,7 +15,7 @@ export default function Destination(){
             <p>“Not all those who wander are lost.”</p>
             <Destinationdata
                 classname="des-1"
-                heading="Rann of Kutch"
+                heading="Rann of Kutch: The White Desert"
                 para="Rann of Kutch is a massive expanse of cracked earth, inland from the sea, that promises to take your breath away. The nothingness for miles is both nerve wracking and stunning with small oasis of water bodies and shrub forests doubling up as homes for pink flamingoes and wild asses. Tribal hamlets with cylindrical mud bhungas (huts) are the epicenter for Kutchi embroidery, tie and dye, leatherwork, pottery, bell metal craft and the famous Rogan painting by the only surviving expert family. Approximately 200 km east of the Rann, is the Little Rann of Kutch, which houses the 4953-sq-km Wild Ass Sanctuary. It homes the only remaining population of the chestnut-coloured Indian wild ass (khur), as well as blue-bulls, blackbuck and chinkara."
                 img1={Raanofkutch}
                 img2={raan}
@@ -24,6 +27,20 @@ export default function Destination(){
                   "
                 img1={kyoto}
                 img2={aboutpic}
+            />
+            <Destinationdata
+                classname="des-3"
+                heading="Paris: City of Love & Cafe Culture"
+                para="Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré."
+                img1={paris1}
+                img2={paris2}
+            />
+            <Destinationdata
+                classname="des-4"
+                heading="Taj Mahal"
+                para="The Taj Mahal, is an Islamic ivory-white marble mausoleum on the right bank of the river Yamuna in the Indian city of Agra. It was commissioned in 1631 by the Mughal emperor Shah Jahan to house the tomb of his favourite wife, Mumtaz Mahal; it also houses the tomb of Shah Jahan himself.The Taj Mahal was designated as a UNESCO World Heritage Site in 1983 for being 'the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage'."
+                img1={tajj}
+                img2={tajmahal}
             />
         </div>
     )
--- src/components/trip.js
+++ src/components/trip.js
@@ -3,6 +3,9 @@ import "./trip.css"
 import maldives from '../Assets/maldives.jpg'
 import pyramids from '../Assets/pyramids.jpg'
 import burj from '../Assets/burj.jpg'
+import sigiriya from '../Assets/sigiriya.jpg'
+import greatwall from '../Assets/greatwallofchina.jpg'
+import everest from '../Assets/everest.jpg'
 
 export default function Trip(){
     return(
@@ -11,16 +14,9 @@ export default function Trip(){
             <p>You can find unique destinations using Google Maps</p>
             <div className="trip-card">
                 <Tripdata
-                    cardimg={maldives}
-                    heading="Maldives"
-                    para="
-                    The Maldives is a nation of islands in the Indian Ocean, that spans across the equator. The country is comprised of 1192 islands that stretch along a length of 871 kilometers. While the country covers an area of approximately 90,000 square kilometers, only 298 square kilometers of that is dry land. The islands are grouped into a double chain of 26 atolls.
-                    "
-                />
-                <Tripdata
-                    cardimg={pyramids}
-                    heading="Pyramids of Giza"
-                    para="The Great Pyramid of Giza is the biggest Egyptian pyramid and the tomb of Fourth Dynasty pharaoh Khufu. Built in the early 26th century BC during a period of around 27 years, the pyramid is the oldest of the Seven Wonders of the Ancient World, and the only one to remain largely intact."
+                    cardimg={everest}
+                    heading="Mt.Everest"
+                    para="Mount Everest is Earth's highest mountain above sea level, located in the Mahalangur Himal sub-range of the Himalayas. The China–Nepal border runs across its summit point. Its elevation of 8,848.86 m was most recently established in 2020 by the Chinese and Nepali authorities."
                 />
                 <Tripdata
                     cardimg={burj}
@@ -29,7 +25,32 @@ export default function Trip(){
 
                     At 828m tall, this magnificent structure is located next to Dubai Mall and has drawn visitors from all over the world since opening in 2010. The unmatched Burj Khalifa view can be taken in from not one but two observations decks – the two-storey At the Top on the 124th and 125th floors, as well as one of the world’s highest observation decks (555m) on the 148th floor."
                 />
-            </div>
+                <Tripdata
+                    cardimg={pyramids}
+                    heading="Pyramids of Giza"
+                    para="The Great Pyramid of Giza is the biggest Egyptian pyramid and the tomb of Fourth Dynasty pharaoh Khufu. Built in the early 26th century BC during a period of around 27 years, the pyramid is the oldest of the Seven Wonders of the Ancient World, and the only one to remain largely intact."
+                />
+                </div>
+                <div className="trip-card">
+                <Tripdata
+                    cardimg={sigiriya}
+                    heading="Sigiriya"
+                    para="Sigiriya or Sinhagiri is an ancient rock fortress located in the northern Matale District near the town of Dambulla in the Central Province, Sri Lanka. It is a site of historical and archaeological significance that is dominated by a massive column of rock approximately 180 m high."
+                />
+                <Tripdata
+                    cardimg={maldives}
+                    heading="Maldives"
+                    para="
+                    The Maldives is a nation of islands in the Indian Ocean, that spans across the equator. The country is comprised of 1192 islands that stretch along a length of 871 kilometers. While the country covers an area of approximately 90,000 square kilometers, only 298 square kilometers of that is dry land. The islands are grouped into a double chain of 26 atolls.
+                    "
+                />
+                <Tripdata
+                    cardimg={greatwall}
+                    heading="Great Wall of China"
+                    para="The Great Wall of China is a series of fortifications that were built across the historical northern borders of ancient Chinese states and Imperial China as protection against various nomadic groups from the Eurasian Steppe."
+                />
+                </div>
+
         </div>
     )
 }
