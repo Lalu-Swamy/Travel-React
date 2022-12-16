484af7448dae3337be7d2a521f51e6e928244ddc
Lalu-Swamy <120311589+Lalu-Swamy@users.noreply.github.com>
2022-12-16 09:07:36 +0000

    my changes

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,7 +6,6 @@ export default function Hero(props){
             <div className="hero-text">
                 <h1>{props.title}</h1>
                 <p>{props.text}</p>
-                <a href={props.url} className={props.btnclass} >{props.btntext}</a>
             </div>
         </div>
 
